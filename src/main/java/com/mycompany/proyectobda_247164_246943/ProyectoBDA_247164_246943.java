137735499da6cfc53289f03eb3c33b2ffd2ea60f
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-02-13 19:10:20 -0700

    projecto de Bases de datos Avanzadas

---
+++ src/main/java/com/mycompany/proyectobda_247164_246943/ProyectoBDA_247164_246943.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectobda_247164_246943;
+
+/**
+ *
+ * @author hisam
+ */
+public class ProyectoBDA_247164_246943 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
