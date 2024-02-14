d6ce246e6e7055296001141aade9f81a16452152
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-02-14 01:35:08 -0700

    BancoMBDominio

---
+++ src/main/java/org/itson/bda/proyectobda_247164_246943/bancoMB/Clientes.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.bda.proyectobda_247164_246943.bancoMB;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hisam
+ */
+public class Clientes {
+
+   
+    private Integer id;
+    private Date fechaNacimiento;
+    private String nombre, apellidoMaterno, apellidoPaterno;
+    private int edad;
+    private int idDomicilio;
+
+    public Clientes() {
+    }
+
+    public Clientes(Date fechaNacimiento, String nombre, String apellidoMaterno, String apellidoPaterno) {
+        this.fechaNacimiento = fechaNacimiento;
+        this.nombre = nombre;
+        this.apellidoMaterno = apellidoMaterno;
+        this.apellidoPaterno = apellidoPaterno;
+    }
+    
+    
+
+    public Clientes(Integer id, Date fechaNacimiento, String nombre, String apellidoMaterno, String apellidoPaterno) {
+        this.id = id;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nombre = nombre;
+        this.apellidoMaterno = apellidoMaterno;
+        this.apellidoPaterno = apellidoPaterno;
+  
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getIdDomicilio() {
+        return idDomicilio;
+    }
+
+    public void setIdDomicilio(int idDomicilio) {
+        this.idDomicilio = idDomicilio;
+    }
+    
+  
+
+    
+    
+    
+}
+
