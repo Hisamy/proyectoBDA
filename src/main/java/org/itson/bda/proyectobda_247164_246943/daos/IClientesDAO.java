46b641e165efc296efef9052915da9a9153a47df
MOCH-CHILA <116768349+MOCH-CHILA@users.noreply.github.com>
2024-02-15 10:04:23 -0600

    Update IClientesDAO.java
    Documentacion IClientesDAO.java

--- src/main/java/org/itson/bda/proyectobda_247164_246943/daos/IClientesDAO.java
+++ src/main/java/org/itson/bda/proyectobda_247164_246943/daos/IClientesDAO.java
@@ -8,10 +8,23 @@ import org.itson.bda.proyectobda_247164_246943.bancoMB.Clientes;
 import org.itson.bda.proyectobda_247164_246943.dtos.ClienteNuevoDTO;
 import org.itson.bda.proyectobda_247164_246943.excepciones.PersistenciaException;
 
-
+/**
+ * La interfaz `IClientesDAO` define las operaciones necesarias para acceder y manipular
+ * datos relacionados con clientes en la base de datos.
+ */
 public interface IClientesDAO {
-    
-    List<Clientes> consultar() throws PersistenciaException;
 
+    /**
+     * Consulta la información de todos los clientes en la base de datos.
+     * @return Una lista de objetos `Clientes`.
+     * @throws PersistenciaException Si ocurre un error al consultar la base de datos.
+     */
+    List<Clientes> consultar() throws PersistenciaException;
+     /**
+     * Agrega un nuevo cliente a la base de datos.
+     * @param clienteNuevo Objeto `ClienteNuevoDTO` que contiene la información del nuevo cliente.
+     * @return Un objeto `Clientes` con la información del cliente recién agregado.
+     * @throws PersistenciaException Si ocurre un error al insertar el cliente en la base de datos.
+     */
     Clientes agregar(ClienteNuevoDTO socioNuevo) throws PersistenciaException;
 }
