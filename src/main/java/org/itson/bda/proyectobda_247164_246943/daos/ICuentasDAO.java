a897a3ef7414ae9a5a854a65fe7272e27b283817
MOCH-CHILA <116768349+MOCH-CHILA@users.noreply.github.com>
2024-02-15 10:09:07 -0600

    Update ICuentasDAO.java
    Documentacion ICuentasDAO.java

--- src/main/java/org/itson/bda/proyectobda_247164_246943/daos/ICuentasDAO.java
+++ src/main/java/org/itson/bda/proyectobda_247164_246943/daos/ICuentasDAO.java
@@ -6,11 +6,23 @@ import org.itson.bda.proyectobda_247164_246943.bancoMB.Cuentas;
 import org.itson.bda.proyectobda_247164_246943.dtos.CuentaNuevaDTO;
 import org.itson.bda.proyectobda_247164_246943.excepciones.PersistenciaException;
 
-
+/**
+* La interfaz `ICuentasDAO` define las operaciones necesarias para acceder y manipular
+* datos relacionados con cuentas en la base de datos.
+*/
 public interface ICuentasDAO {
- 
+    /**
+    * Consulta la información de todas las cuentas en la base de datos.
+    * @return Una lista de objetos `Cuentas`.
+    * @throws PersistenciaException Si ocurre un error al consultar la base de datos.
+    */
     List<Cuentas> consultar() throws PersistenciaException;
-
+    /**
+    * Agrega una nueva cuenta a la base de datos.
+    * @param cuentaNueva Objeto `CuentaNuevaDTO` que contiene la información de la nueva cuenta.
+    * @return Un objeto `Cuentas` con la información de la cuenta recién agregada.
+    * @throws PersistenciaException Si ocurre un error al insertar la cuenta en la base de datos.
+    */
     Cuentas agregar(CuentaNuevaDTO cuentaNueva) throws PersistenciaException;
 }
 
