a4be1c20b04aed3a9a161d35f17e30118cf2e985
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-02-15 02:57:03 -0700

    BancoMBPersistencia

--- src/main/java/org/itson/bda/proyectobda_247164_246943/daos/RetirosSinCuentaDAO.java
+++ src/main/java/org/itson/bda/proyectobda_247164_246943/daos/RetirosSinCuentaDAO.java
@@ -14,8 +14,6 @@ import java.util.logging.Logger;
 import org.itson.bda.proyectobda_247164_246943.bancoMB.Cuentas;
 import org.itson.bda.proyectobda_247164_246943.bancoMB.RetirosSinCuenta;
 import org.itson.bda.proyectobda_247164_246943.conexiones.IConexion;
-import static org.itson.bda.proyectobda_247164_246943.daos.CuentasDAO.logger;
-import org.itson.bda.proyectobda_247164_246943.dtos.CuentaNuevaDTO;
 import org.itson.bda.proyectobda_247164_246943.dtos.RetiroSinCuentaNuevoDTO;
 import org.itson.bda.proyectobda_247164_246943.excepciones.PersistenciaException;
 
