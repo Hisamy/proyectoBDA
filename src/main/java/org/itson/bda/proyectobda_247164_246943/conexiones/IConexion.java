a3f3d1c970296a8772d19d5dc24a04a41b0ba5b4
MOCH-CHILA <116768349+MOCH-CHILA@users.noreply.github.com>
2024-02-17 02:41:59 -0600

    Update IConexion.java
    Documentacion de IConexion en la clase principal

--- src/main/java/org/itson/bda/proyectobda_247164_246943/conexiones/IConexion.java
+++ src/main/java/org/itson/bda/proyectobda_247164_246943/conexiones/IConexion.java
@@ -4,8 +4,16 @@ package org.itson.bda.proyectobda_247164_246943.conexiones;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-
+/**
+ * La interfaz `IConexion` define un contrato para obtener conexiones a una base de datos.
+ */
 public interface IConexion {
+
+       /**
+        * Obtiene una conexión a la base de datos.
+        * @return La conexión establecida.
+        * @throws SQLException Si hay un error al establecer la conexión.
+        */
         Connection obtenerConexion() throws SQLException;
 
 }
