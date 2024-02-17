df5aca917c993801506c1341be8a91e80d6b48de
MOCH-CHILA <116768349+MOCH-CHILA@users.noreply.github.com>
2024-02-17 02:05:13 -0600

    Update BancoMB.java
    Documentacion BancoMB en la clase principal.

--- src/main/java/org/itson/bda/BancoMB/bancoMB/BancoMB.java
+++ src/main/java/org/itson/bda/BancoMB/bancoMB/BancoMB.java
@@ -11,15 +11,42 @@ import org.itson.bda.proyectobda_247164_246943.daos.RetirosSinCuentaDAO;
 
 public class BancoMB {
 
-   
+    /**
+    * @param args Clase principal de BancoMB.
+    */
     public static void main(String[] args) {
+
+        /**
+        * Credenciales y detalles de conexión a la base de datos (URL, usuario, contraseña).
+        */
         String cadenaConexion = "jdbc:mysql://localhost/banco";
         String usuario = "root";
         String password = "cinco123";
+
+        /**
+        * Instancia de la interfaz IConexion implementando Conexion.
+        */
         IConexion conexion = new Conexion(cadenaConexion, usuario, password);
+
+        /**
+        * Instancia de la interfaz IClientesDAO implementando ClientesDAO, pasando la instancia de conexión.
+        */
         IClientesDAO clientesDAO = new ClientesDAO(conexion);
+
+        /**
+        * Instancia de la interfaz IRetiroSinCuentaDAO implementando la clase RetirosSinCuentaDAO.
+        * Se utiliza para realizar operaciones relacionadas con retiros sin cuenta en la base de datos.
+        */
         IRetiroSinCuentaDAO retirosSinCuentaDAO = new RetirosSinCuentaDAO(conexion);
+
+       /**
+        * Instancia de la clase RetiroSinCuentaForm se le pasa la instancia de IClientesDAO para operaciones relacionadas con clientes.
+        */
         RetiroSinCuentaForm clientesForm = new RetiroSinCuentaForm(clientesDAO);
+
+        /**
+         * Hace visible el formulario de RetiroSinCuentaForm en la interfaz gráfica.
+         */
         clientesForm.setVisible(true);
         
     
