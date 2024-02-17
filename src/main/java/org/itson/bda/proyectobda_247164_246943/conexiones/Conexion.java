690e917be2b26032f86525f22adf11b0013d2a8d
MOCH-CHILA <116768349+MOCH-CHILA@users.noreply.github.com>
2024-02-17 02:44:16 -0600

    Update Conexion.java
    Documentacion de Conexion.java en la clase principal.

--- src/main/java/org/itson/bda/proyectobda_247164_246943/conexiones/Conexion.java
+++ src/main/java/org/itson/bda/proyectobda_247164_246943/conexiones/Conexion.java
@@ -7,22 +7,41 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
+/**
+ * La clase `Conexion` implementa la interfaz `IConexion` y se encarga de establecer
+ * y proporcionar conexiones la base de datos utilizando JDBC.
+ */
 public class Conexion implements IConexion{
-    
+    //Atributos de la clase.
     final String cadenaConexion;
     final String usuario;
     final String password;
     static final Logger logger = Logger.getLogger(Conexion.class.getName());
 
+
+   /**
+    * Constructor de la clase `Conexion`.
+    * @param cadenaConexion La cadena de conexión a la base de datos.
+    * @param usuario El nombre de usuario para la conexión.
+    * @param password La contraseña para la conexión.
+    */
     public Conexion(String cadenaConexion, String usuario, String password) {
         this.cadenaConexion = cadenaConexion;
         this.usuario = usuario;
         this.password = password;
     }
 
+   /**
+    * Obtiene una conexión a la base de datos.
+    * @return La conexión establecida.
+    * @throws SQLException Si hay un error al establecer la conexión.
+    */
     @Override
     public Connection obtenerConexion() throws SQLException {
+          // Establecer la conexión utilizando JDBC
         Connection conexion = DriverManager.getConnection(cadenaConexion, usuario, password);
+         // Registrar el evento de conexión exitosa en el registro de log
         logger.log(Level.INFO, "Conexion establecida {0}", cadenaConexion);
         return conexion;
     }
