fe79b7a4931e637e3284eadd473c47a0c7b5d37a
MOCH-CHILA <116768349+MOCH-CHILA@users.noreply.github.com>
2024-02-17 02:08:34 -0600

    Update Acciones.java
    Documentacion acciones en la clase principal.

--- src/main/java/org/itson/bda/BancoMB/bancoMB/Acciones.java
+++ src/main/java/org/itson/bda/BancoMB/bancoMB/Acciones.java
@@ -1,8 +1,18 @@
 
 package org.itson.bda.BancoMB.bancoMB;
 
-
+/**
+* Declara una nueva enumeracion llamado Acciones.
+*/
 public enum Acciones {
+
+    /**
+    * Constante que realizar acciones correspondientes a aceptar.
+    */
     ACEPTAR, 
+
+    /**
+    * Constante que realizar acciones correspondientes a cancelar
+    */
     CANCELAR
 }
